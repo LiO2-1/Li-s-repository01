ac3f94e7def183ec9c00443dff042b25dee031ab
LiO2-1 <1933619092@qq.com>
2026-09-07 21:25:44 +0800

    API第二次学习

---
+++ day14-API-third-app/src/com/qiyuesan/System01/Demo.java
@@ -0,0 +1,10 @@
+package com.qiyuesan.System01;
+
+import java.io.IOException;
+
+public class Demo {
+    public static void main(String[] args) throws IOException {
+        Runtime r = Runtime.getRuntime();
+        r.exec("E:\\常用软件\\Wuthering Waves\\launcher.exe");
+    }
+}

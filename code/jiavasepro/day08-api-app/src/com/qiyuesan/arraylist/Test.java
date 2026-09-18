2f5556c44021c7e18a75c19bee0acd264321b9cd
LiO2-1 <1933619092@qq.com>
2026-09-18 23:50:12 +0800

    补回丢失的代码

--- code/jiavasepro/day08-api-app/src/com/qiyuesan/arraylist/Test.java
+++
@@ -1,9 +0,0 @@
-package com.qiyuesan.arraylist;
-
-public class Test {
-    //商品上架
-    public static void main(String[] args) {
-        FoodOperate food = new FoodOperate();
-        food.start();
-    }
-}
